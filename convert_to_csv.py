9e847cbcf39d51663b4976bbde84b046975b7ff2
David Enoma <davidenoma@gmail.com>
2022-11-07 13:58:07 -0700

    print length of fpath

---
+++ convert_to_csv.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import sys
+
+fpath = sys.argv[1]
+print(len(fpath))
+if len(sys.argv) > 2:
+    key = sys.argv[2]
+    df = pd.read_hdf(fpath, key=key)
+else:
+    df = pd.read_hdf(fpath)
+
+df.to_csv(sys.stdout, index=False)
