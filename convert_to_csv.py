6bbddb1b455aa8919e0bfc1de1c778949aeab110
David Enoma <davidenoma@gmail.com>
2022-11-10 14:41:43 -0700

    using h5py for hdf5 file write

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -1,11 +1,22 @@
-import pandas as pd
+import h5py
 import sys
 fpath = sys.argv[1]
-print(len(fpath),)
-if len(sys.argv) > 2:
-    key = sys.argv[2]
-    df = pd.read_hdf(fpath, key=key)
-else:
-    df = pd.read_hdf(fpath)
 
-df.to_csv(sys.stdout, index=False)
+with h5py.File(fpath, "r") as f:
+    # Print all root level object names (aka keys)
+    # these can be group or dataset names
+    print("Keys: %s" % f.keys())
+    # get first object name/key; may or may NOT be a group
+    a_group_key = list(f.keys())[0]
+
+    # get the object type for a_group_key: usually group or dataset
+    print(type(f[a_group_key]))
+    # If a_group_key is a group name,
+    # this gets the object names in the group and returns as a list
+    data = list(f[a_group_key])
+    # If a_group_key is a dataset name,
+    # this gets the dataset values and returns as a list
+    data = list(f[a_group_key])
+    # preferred methods to get dataset values:
+    ds_obj = f[a_group_key]      # returns as a h5py dataset object
+    ds_arr = f[a_group_key][()]  # returns as a numpy array
