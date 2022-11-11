8813a3e8a71e74f2f63245c790672642082b3031
David Enoma <davidenoma@gmail.com>
2022-11-11 14:17:13 -0700

    update the datatype

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -10,7 +10,7 @@ with h5py.File(fpath, "r") as f:
     # these can be group or dataset names
     print("Keys: %s" % f.keys())
     group_keys = list(f.keys())
-    ds_arr = np.empty(f[group_keys[0]].shape[0], len(group_keys))
+    ds_arr = np.empty([f[group_keys[0]].shape[0], len(group_keys)])
     i = 0
     for key in group_keys():
         ds_arr[:, i] = f[key]
