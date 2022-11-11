ed3470a31590bf5b3946f1105ec9526fd3bf447a
David Enoma <davidenoma@gmail.com>
2022-11-11 16:50:19 -0700

    trying empty tuple to create dataset

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -10,19 +10,19 @@ with h5py.File(fpath, "r") as f:
     group_keys = list(f.keys())
     #Keys: <KeysViewHDF5 ['SAD', 'SAR', 'alt', 'chr', 'pos', 'ref', 'snp', 'target_ids', 'target_labels']
     #getting the shape for SAD scores.
-    snp_activity_difference = np.empty([f[group_keys[0]].shape[0], f[group_keys[0]].shape[1]])
-    snps = np.empty([f[group_keys[2]].shape[0],] )
-    targets = np.empty([f[group_keys[7]].shape[0], ])
-    print(snp_activity_difference.shape,snps.shape)
+    # snp_activity_difference = np.empty([f[group_keys[0]].shape[0], f[group_keys[0]].shape[1]])
+    # snps = np.empty([f[group_keys[2]].shape[0],] )
+    # targets = np.empty([f[group_keys[7]].shape[0], ])
+    # print(snp_activity_difference.shape,snps.shape)
     i = 0
     for key in group_keys:
         print("Key for: ",group_keys[i])
         if i < 2:
-            snp_activity_difference[:, :] = f[key]
+            snp_activity_difference = f[key][()]
         if i >= 2 & i < 7:
-            snps[:,] = f[key]
+            snps = f[key][()]
         if i > 7:
-            targets[:,] = f[key]
+            targets = f[key][()]
         i+=1
 f.close()
 chr_number = fpath.split(".")[-2]
