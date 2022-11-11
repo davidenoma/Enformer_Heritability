10670ffa2cc13dfcb304f65986b650fb294f19f8
David Enoma <davidenoma@gmail.com>
2022-11-11 16:37:56 -0700

    updating the shapes

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -13,6 +13,7 @@ with h5py.File(fpath, "r") as f:
     snp_activity_difference = np.empty([f[group_keys[0]].shape[0], f[group_keys[0]].shape[1]])
     snps = np.empty([f[group_keys[2]].shape[0],] )
     targets = np.empty([f[group_keys[7]].shape[0], ])
+    print(snp_activity_difference.shape,snps.shape)
     i = 0
     for key in group_keys:
         print("Key for: ",group_keys[i])
