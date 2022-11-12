a896304e77abbd3e510df2efb5c4000b99ca9088
David Enoma <davidenoma@gmail.com>
2022-11-11 17:58:41 -0700

    checking np_ concatenate

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -12,18 +12,19 @@ with h5py.File(fpath, "r") as f:
     #getting the shape for SAD scores.
     snp_activity_difference = np.empty([f[group_keys[0]].shape[0], f[group_keys[0]].shape[1]])
     snps = np.empty([f[group_keys[2]].shape[0],] )
-    targets = np.empty([f[group_keys[7]].shape[0], ])
+    targets = np.empty([f[group_keys[7]].shape[0],])
     i = 0
     for key in group_keys:
         print("Key for: ",group_keys[i])
         print(f[key].shape,key)
-        # if i < 2:
-        #     snp_activity_difference = f[key][()]
-        # if i >= 2 & i < 7:
-        #     snps = f[key][()]
-        # if i > 7:
-        #     targets = f[key][()]
+        if i < 2:
+            snp_activity_difference = np.concatenate(snp_activity_difference,f[key][()])
+        if i >= 2 & i < 7:
+            snps = f[key][()]
+        if i > 7:
+            targets = f[key][()]
         i+=1
+print(snp_activity_difference.shape)
 f.close()
 chr_number = fpath.split(".")[-2]
 np.savetxt("SA_"+chr_number + ".csv", snp_activity_difference, delimiter=",")
