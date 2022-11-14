7bace15fda15bdcb44a1e0826d86f11f0eaf47c4
David Enoma <davidenoma@gmail.com>
2022-11-13 18:11:25 -0700

    employing pandas

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -1,6 +1,7 @@
 import h5py
 import sys
 import numpy as np
+import pandas as pd
 fpath = sys.argv[1]
 snps = ""
 targets = ""
@@ -19,13 +20,16 @@ with h5py.File(fpath, "r") as f:
     alt = f['alt'][()]
     chr = f['chr'][()]
     pos = f['pos'][()]
-    print("snp",snp.shape)
+
+    print("snp",snp.type)
     target_ids = f['target_ids'][()]
     target_labels = f['target_labels'][()]
     print('target_ids',target_ids.shape)
-    snps = np.ma.stack((alt,chr,pos,ref,snp))
+    snps = np.vstack((alt,chr,pos,ref,snp))
+    snps  = pd.DataFrame(snps)
     # snps = np.empty([f['alt'].shape[0],] )
-    targets = np.ma.vstack((target_ids,target_labels))
+    targets = np.vstack((target_ids,target_labels))
+    targets = pd.DataFrame(targets)
     # i = 2
     # for key in group_keys:
     #     print(i)
@@ -41,9 +45,7 @@ chr_number = fpath.split(".")[-2]
 print("Done writing..."+"SADs"+chr_number)
 # np.savetxt("SARs"+chr_number + ".csv", SARs, delimiter=",")
 print("Done writing..."+"SADs"+chr_number)
-# np.savetxt("snps"+chr_number + ".csv", snps, delimiter=",")
+pd.DataFrame.to_csv(snps,"snps"+chr_number + ".csv" )
 print("Done writing..."+"snps"+chr_number)
-
-
-np.savetxt("targets"+chr_number + ".csv", targets, delimiter=",")
+pd.DataFrame.to_csv(targets,"targets"+chr_number + ".csv" )
 print("Done writing..."+"targets"+chr_number)
