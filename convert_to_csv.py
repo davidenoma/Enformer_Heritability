f9dc1598edbd644e53007c2d649dedbd5ea4d7b3
David Enoma <davidenoma@gmail.com>
2022-11-15 11:04:06 -0700

    writing only SAR because of memory constraint

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -11,9 +11,9 @@ with h5py.File(fpath, "r") as f:
     #Keys: <KeysViewHDF5 ['SAD', 'SAR', 'alt', 'chr', 'pos', 'ref', 'snp', 'target_ids', 'target_labels']
     #There are Datasets and there are Groups. This is composed of datasets.
     #getting the shape for SAD scores.
-    SADs = f['SAD'][()]
+    # SADs = f['SAD'][()]
     SARs = f['SAR'][()]
-    print("SAR AND SAD",SARs.shape, SADs.shape)
+    print("SAR ",SARs.shape)
     snp = f['snp'].asstr()[()]
     ref = f['ref'].asstr()[()]
     alt = f['alt'].asstr()[()]
@@ -35,8 +35,8 @@ print("snps and targets shape",snps.shape,targets.shape)
 f.close()
 chr_number = fpath.split(".")[-2]
 
-np.savetxt("./enformer_variant_scores_csv/"+chr_number+"/SADs" + ".csv", SADs, delimiter=",")
-print("Done writing..."+"SADs"+chr_number)
+# np.savetxt("./enformer_variant_scores_csv/"+chr_number+"/SADs" + ".csv", SADs, delimiter=",")
+# print("Done writing..."+"SADs"+chr_number)
 np.savetxt("./enformer_variant_scores_csv/"+chr_number+"/SARs" + ".csv", SARs, delimiter=",")
 print("Done writing..."+"SADs"+chr_number)
 pd.DataFrame.to_csv(snps,"./enformer_variant_scores_csv/"+chr_number+"/snps"+chr_number + ".csv" )
